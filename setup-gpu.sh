17a663e9ee33a849770584bd9d333593a092a217
hossein-khalilian <hse.khalilian08@gmail.com>
2025-07-01 08:52:22 +0330

    add first setup script

---
+++ setup-gpu.sh
@@ -0,0 +1,16 @@
+mkdir -p ~/.config
+cd ~/.config
+git clone https://github.com/hossein-khalilian/nvim-config.git nvim
+cd nvim
+./pre-installation.sh
+source ~/.bashrc
+
+mkdir -p ~/projects/hse/git
+cd ~/projects/hse/git
+git clone https://github.com/hossein-khalilian/jupyterlab-compose.git
+cd jupyterlab-compose/
+docker compose -f docker-compose-gpu.yml up -d
+
+virtualenv ~/projects/hse/venv2
+source ~/projects/hse/venv2/bin/activate
+
