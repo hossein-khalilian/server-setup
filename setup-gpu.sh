bceb8ad6dbcd4896c23da05a52ce36d54b40e167
hossein-khalilian <hse.khalilian08@gmail.com>
2025-07-01 09:17:43 +0330

    make it safe to run multiple times

--- setup-gpu.sh
+++ setup-gpu.sh
@@ -1,26 +1,25 @@
 sudo apt update
 sudo apt --fix-broken install -y
-sudo apt autoremove
+sudo apt autoremove -y
 sudo apt install -y tmux curl
 
-tmux new -s setup
+tmux new -As setup
 
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/hossein-khalilian/server-setup/main/.tmux.conf
 tmux source-file ~/.tmux.conf
 
 mkdir -p ~/.config
 cd ~/.config
-git clone https://github.com/hossein-khalilian/nvim-config.git nvim
+[ ! -d "nvim" ] && git clone https://github.com/hossein-khalilian/nvim-config.git nvim
 cd nvim
-./pre-installation.sh
+./pre-installation.sh || true
 source ~/.bashrc
 
 mkdir -p ~/projects/hse/git
 cd ~/projects/hse/git
-git clone https://github.com/hossein-khalilian/jupyterlab-compose.git
+[ ! -d "jupyterlab-compose" ] && git clone https://github.com/hossein-khalilian/jupyterlab-compose.git
 cd jupyterlab-compose/
 docker compose -f docker-compose-gpu.yml up -d
 
-virtualenv ~/projects/hse/venv2
-source ~/projects/hse/venv2/bin/activate
-
+[ ! -d "$HOME/projects/hse/venv2" ] && virtualenv "$HOME/projects/hse/venv2"
+source "$HOME/projects/hse/venv2/bin/activate"
